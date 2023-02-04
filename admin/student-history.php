a0a3373c4b87c2c828577d987bb9c070743545af
Naveen Rajan <naveentherocker1234@gmail.com>
2023-02-05 03:48:30 +0530

    Fix SQL Injection

--- admin/student-history.php
+++ admin/student-history.php
@@ -100,7 +100,7 @@ header('location:reg-students.php');
 <?php 
 
 $sql = "SELECT tblstudents.StudentId ,tblstudents.FullName,tblstudents.EmailId,tblstudents.MobileNumber,tblbooks.BookName,tblbooks.ISBNNumber,tblissuedbookdetails.IssuesDate,tblissuedbookdetails.ReturnDate,tblissuedbookdetails.id as rid,tblissuedbookdetails.fine,tblissuedbookdetails.RetrunStatus,tblbooks.id as bid,tblbooks.bookImage from  tblissuedbookdetails join tblstudents on tblstudents.StudentId=tblissuedbookdetails.StudentId join tblbooks on tblbooks.id=tblissuedbookdetails.BookId where tblstudents.StudentId='$sid' ";
-$query = $dbh -> prepare($sql);
+$query = $dbh -> fetch_array($sql);
 $query->execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
 $cnt=1;
